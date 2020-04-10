5787415d46b056d662bf9521112e7aee88bfe901
Etienne Warneck <etienne.warneck@gmail.com>
2020-04-10 12:06:53 -0400

    db.js added values in .connect

--- config/db.js
+++ config/db.js
@@ -6,14 +6,15 @@ const connectDB = async () => {
     try {
         await mongoose
             .connect(db, {
-                // useNewUrlParser: true, //mongoose
-                // useCreateIndex: true,
-                // useFindAndModify: false
+                useNewUrlParser: true,
+                useCreateIndex: true,
+                useFindAndModify: false,
+                useUnifiedTopology: true,
             });
         console.log('MongoDB Connected...');
     }
     catch (err) {
-        console.log(err.message);
+        console.error(err.message);
         process.exit(1) //with failure
     }
 };
