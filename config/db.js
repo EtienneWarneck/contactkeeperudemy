c02287eebd3fe77aa59e33409a17d33139af259a
Etienne Warneck <etienne.warneck@gmail.com>
2020-04-10 12:18:54 -0400

    deploy

--- config/db.js
+++ config/db.js
@@ -4,16 +4,15 @@ const db = config.get('mongoURI'); //grab Uniform Resource Identifier to Mongo
 
 const connectDB = async () => {
     try {
-        await mongoose
-            .connect(db, {
-                useNewUrlParser: true,
-                useCreateIndex: true,
-                useFindAndModify: false,
-                useUnifiedTopology: true,
-            });
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true,
+        });
+
         console.log('MongoDB Connected...');
-    }
-    catch (err) {
+    } catch (err) {
         console.error(err.message);
         process.exit(1) //with failure
     }
