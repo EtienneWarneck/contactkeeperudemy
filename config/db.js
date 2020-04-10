53fd7bc49a1ef469dfd3b94a7c9065225c8b8450
Etienne Warneck <etienne.warneck@gmail.com>
2020-04-10 12:57:07 -0400

    db.js copy paste

--- config/db.js
+++ config/db.js
@@ -1,40 +1,21 @@
 const mongoose = require('mongoose');
-const config = require('config'); //access default
-const db = config.get('mongoURI'); //grab Uniform Resource Identifier to Mongo
+const config = require('config');
+const db = config.get('mongoURI');
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
-            useUnifiedTopology: true,
-        });
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
 
-        console.log('MongoDB Connected...');
-    } catch (err) {
-        console.error(err.message);
-        process.exit(1) //with failure
-    }
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
-
-//SAME AS:----------------------------------------------------------------
-// const connectDB = () => {
-//     mongoose
-//         .connect(db, {
-//             useNewUrlParser: true,
-//             useCreateIndex: true,
-//             useFindAndModify: false
-//         })
-//         .then(() => console.log('MongoDB Connected'))
-//         .catch(err => {
-//         console.error(err.message);
-//         process.exit(1)
-//     });
-// };
-
-// module.exports = connectDB;
-
-
